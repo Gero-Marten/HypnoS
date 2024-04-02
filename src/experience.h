993788091a4f979e43dd42732998c7193f3476d6
Marco Zerbinati <zerbinati.marco@gmail.com>
2024-04-03 00:07:58 +0200

    Add improved learning code

--- src/experience.h
+++ src/experience.h
@@ -16,213 +16,203 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef __EXPERIENCE_H__
-#define __EXPERIENCE_H__
-
-#include "types.h"
+#ifndef EXPERIENCE_H_INCLUDED
+#define EXPERIENCE_H_INCLUDED
 
 using namespace std;
+using namespace Stockfish;
 
-#define EXP_MIN_DEPTH ((Depth)4)
+constexpr inline Depth EXP_MIN_DEPTH = 4;
 
-namespace Experience
-{
-    namespace V1
-    {
-        struct ExpEntry
-        {
-            Stockfish::Key     key;        //8 bytes
-            Stockfish::Move    move;       //4 bytes
-            Stockfish::Value   value;      //4 bytes
-            Stockfish::Depth   depth;      //4 bytes
-            uint8_t padding[4];            //4 bytes
+namespace Experience {
+namespace V1 {
+struct ExpEntry {
+    Key     key;           //8 bytes
+    Move    move;          //4 bytes
+    Value   value;         //4 bytes
+    Depth   depth;         //4 bytes
+    uint8_t padding[4]{};  //4 bytes
 
-            ExpEntry() = delete;
-            ExpEntry(const ExpEntry& exp) = delete;
-            ExpEntry& operator =(const ExpEntry& exp) = delete;
+              ExpEntry()                     = delete;
+              ExpEntry(const ExpEntry& exp)  = delete;
+    ExpEntry& operator=(const ExpEntry& exp) = delete;
 
-            explicit ExpEntry(Stockfish::Key k, Stockfish::Move m, Stockfish::Value v, Stockfish::Depth d)
-            {
-                key = k;
-                move = m;
-                value = v;
-                depth = d;
-                padding[0] = padding[2] = 0x00;
-                padding[1] = padding[3] = 0xFF;
-            }
-
-            void merge(const ExpEntry* exp)
-            {
-                assert(key == exp->key);
-                assert(move == exp->move);
-
-                if (depth > exp->depth)
-                    return;
-
-                if (depth == exp->depth)
-                {
-                    value = (value + exp->value) / 2;
-                }
-                else
-                {
-                    value = exp->value;
-                    depth = exp->depth;
-                }
-            }
-
-            int compare(const ExpEntry* exp) const
-            {
-                int v = value * std::max(depth / 5, 1) - exp->value * std::max(exp->depth / 5, 1);
-                if (!v)
-                    v = depth - exp->depth;
-
-                return v;
-            }
-        };
-
-        static_assert(sizeof(ExpEntry) == 24);
+    explicit ExpEntry(const Key k, const Move m, const Value v, const Depth d) {
+        key        = k;
+        move       = m;
+        value      = v;
+        depth      = d;
+        padding[0] = padding[2] = 0x00;
+        padding[1] = padding[3] = 0xFF;
     }
 
-    namespace V2
-    {
-        struct ExpEntry
+    void merge(const ExpEntry* exp) {
+        assert(key == exp->key);
+        assert(move == exp->move);
+
+        if (depth > exp->depth)
+            return;
+
+        if (depth == exp->depth)
         {
-            Stockfish::Key     key;        //8 bytes
-            Stockfish::Move    move;       //4 bytes
-            Stockfish::Value   value;      //4 bytes
-            Stockfish::Depth   depth;      //4 bytes
-            uint16_t           count;      //2 bytes (A scaled version of count)
-            uint8_t padding[2];            //2 bytes
-
-            ExpEntry() = delete;
-            ExpEntry(const ExpEntry& exp) = delete;
-            ExpEntry& operator =(const ExpEntry& exp) = delete;
-
-            explicit ExpEntry(Stockfish::Key k, Stockfish::Move m, Stockfish::Value v, Stockfish::Depth d) : ExpEntry(k, m, v, d, 1) {}
-
-            explicit ExpEntry(Stockfish::Key k, Stockfish::Move m, Stockfish::Value v, Stockfish::Depth d, uint16_t c)
-            {
-                key = k;
-                move = m;
-                value = v;
-                depth = d;
-                count = c;
-                padding[0] = padding[1] = 0x00;
-            }
-
-            void merge(const ExpEntry* exp)
-            {
-                assert(key == exp->key);
-                assert(move == exp->move);
-
-                //Merge the count
-                count = (uint16_t)std::min((uint32_t)count + (uint32_t)exp->count, (uint32_t)std::numeric_limits<uint16_t>::max());
-
-                //Merge value and depth if 'exp' is better or equal
-                if (depth > exp->depth)
-                    return;
-
-                if (depth == exp->depth)
-                {
-                    value = (value + exp->value) / 2;
-                }
-                else
-                {
-                    value = exp->value;
-                    depth = exp->depth;
-                }
-            }
-
-            int compare(const ExpEntry* exp) const
-            {
-                int v = value * std::max(depth / 10, 1) * std::max(count / 3, 1) - exp->value * std::max(exp->depth / 10, 1) * std::max(exp->count / 3, 1);
-                if (v) return v;
-
-                v = count - exp->count;
-                if (v) return v;
-
-                v = depth - exp->depth;
-                return v;
-            }
-        };
-
-        static_assert(sizeof(ExpEntry) == 24);
+            value = (value + exp->value) / 2;
+        }
+        else
+        {
+            value = exp->value;
+            depth = exp->depth;
+        }
     }
 
-    namespace Current = V2;
+    int compare(const ExpEntry* exp) const {
+        int v = value * std::max(depth / 5, 1) - exp->value * std::max(exp->depth / 5, 1);
+        if (!v)
+            v = depth - exp->depth;
 
-    //Experience structure
-    struct ExpEntryEx : public Current::ExpEntry
-    {
-        ExpEntryEx* next = nullptr;
+        return v;
+    }
+};
 
-        ExpEntryEx() = delete;
-        ExpEntryEx(const ExpEntryEx& exp) = delete;
-        ExpEntryEx& operator =(const ExpEntryEx& exp) = delete;
-
-        explicit ExpEntryEx(Stockfish::Key k, Stockfish::Move m, Stockfish::Value v, Stockfish::Depth d, uint8_t c) : Current::ExpEntry(k, m, v, d, c) {}
-
-        ExpEntryEx* find(Stockfish::Move m) const
-        {
-            ExpEntryEx* exp = const_cast<ExpEntryEx*>(this);
-            do
-            {
-                if (exp->move == m)
-                    return exp;
-
-                exp = exp->next;
-            } while (exp);
-
-            return nullptr;
-        }
-
-        ExpEntryEx* find(Stockfish::Move mv, Stockfish::Depth minDepth) const
-        {
-            ExpEntryEx* temp = const_cast<ExpEntryEx*>(this);
-            do
-            {
-                if ((Stockfish::Move)temp->move == mv)
-                {
-                    if (temp->depth < minDepth)
-                        temp = nullptr;
-
-                    break;
-                }
-
-                temp = temp->next;
-            } while (temp);
-
-            return temp;
-        }
-
-        std::pair<int, bool> quality(Stockfish::Position& pos, int evalImportance) const;
-    };
+static_assert(sizeof(ExpEntry) == 24);
 }
 
-namespace Experience
-{
-    void init();
-    bool enabled();
+namespace V2 {
+struct ExpEntry {
+    Key      key;           //8 bytes
+    Move     move;          //4 bytes
+    Value    value;         //4 bytes
+    Depth    depth;         //4 bytes
+    uint16_t count;         //2 bytes (A scaled version of count)
+    uint8_t  padding[2]{};  //2 bytes
 
-    void unload();
-    void save();
+              ExpEntry()                     = delete;
+              ExpEntry(const ExpEntry& exp)  = delete;
+    ExpEntry& operator=(const ExpEntry& exp) = delete;
 
-    void wait_for_loading_finished();
+    explicit ExpEntry(const Key k, const Move m, const Value v, const Depth d) :
+        ExpEntry(k, m, v, d, 1) {}
 
-    const ExpEntryEx* probe(Stockfish::Key k);
+    explicit ExpEntry(const Key k, const Move m, const Value v, const Depth d, const uint16_t c) {
+        key        = k;
+        move       = m;
+        value      = v;
+        depth      = d;
+        count      = c;
+        padding[0] = padding[1] = 0x00;
+    }
 
-    void defrag(int argc, char* argv[]);
-    void merge(int argc, char* argv[]);
-    void show_exp(Stockfish::Position& pos, bool extended);
-    void convert_compact_pgn(int argc, char* argv[]);
+    void merge(const ExpEntry* exp) {
+        assert(key == exp->key);
+        assert(move == exp->move);
 
-    void pause_learning();
-    void resume_learning();
-    bool is_learning_paused();
+        //Merge the count
+        count = static_cast<uint16_t>(
+          std::min(static_cast<uint32_t>(count) + static_cast<uint32_t>(exp->count),
+                   static_cast<uint32_t>(std::numeric_limits<uint16_t>::max())));
 
-    void add_pv_experience(Stockfish::Key k, Stockfish::Move m, Stockfish::Value v, Stockfish::Depth d);
-    void add_multipv_experience(Stockfish::Key k, Stockfish::Move m, Stockfish::Value v, Stockfish::Depth d);
+        //Merge value and depth if 'exp' is better or equal
+        if (depth > exp->depth)
+            return;
+
+        if (depth == exp->depth)
+        {
+            value = (value + exp->value) / 2;
+        }
+        else
+        {
+            value = exp->value;
+            depth = exp->depth;
+        }
+    }
+
+    int compare(const ExpEntry* exp) const {
+        int v = value * std::max(depth / 10, 1) * std::max(count / 3, 1)
+              - exp->value * std::max(exp->depth / 10, 1) * std::max(exp->count / 3, 1);
+        if (v)
+            return v;
+
+        v = count - exp->count;
+        if (v)
+            return v;
+
+        v = depth - exp->depth;
+        return v;
+    }
+};
+
+static_assert(sizeof(ExpEntry) == 24);
 }
 
-#endif //__EXPERIENCE_H__
+namespace Current = V2;
 
+//Experience structure
+struct ExpEntryEx: Current::ExpEntry {
+    ExpEntryEx* next = nullptr;
+
+                ExpEntryEx()                      = delete;
+                ExpEntryEx(const ExpEntryEx& exp) = delete;
+    ExpEntryEx& operator=(const ExpEntryEx& exp)  = delete;
+
+    explicit ExpEntryEx(const Key k, const Move m, const Value v, const Depth d, const uint8_t c) :
+        ExpEntry(k, m, v, d, c) {}
+
+    [[nodiscard]] ExpEntryEx* find(const Move m) const {
+        auto* exp = const_cast<ExpEntryEx*>(this);
+        do
+        {
+            if (exp->move == m)
+                return exp;
+
+            exp = exp->next;
+        } while (exp);
+
+        return nullptr;
+    }
+
+    [[nodiscard]] ExpEntryEx* find(const Move mv, const Depth minDepth) const {
+        auto* temp = const_cast<ExpEntryEx*>(this);
+        do
+        {
+            if (temp->move == mv)
+            {
+                if (temp->depth < minDepth)
+                    temp = nullptr;
+
+                break;
+            }
+
+            temp = temp->next;
+        } while (temp);
+
+        return temp;
+    }
+
+    std::pair<int, bool> quality(Position& pos, int evalImportance) const;
+};
+}
+
+namespace Experience {
+void init();
+bool enabled();
+
+void unload();
+void save();
+
+void wait_for_loading_finished();
+
+const ExpEntryEx* probe(Key k);
+
+void defrag(int argc, char* argv[]);
+void merge(int argc, char* argv[]);
+void show_exp(Position& pos, bool extended);
+void convert_compact_pgn(int argc, char* argv[]);
+
+void pause_learning();
+void resume_learning();
+bool is_learning_paused();
+
+void add_pv_experience(Key k, Move m, Value v, Depth d);
+void add_multipv_experience(Key k, Move m, Value v, Depth d);
+}
+
+#endif  //EXPERIENCE_H_INCLUDED
