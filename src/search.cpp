88899764825be7a31ac0329101cc1d54f26f6324
Marco Zerbinati <zerbinati.marco@gmail.com>
2024-05-10 15:49:21 +0200

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1378,7 +1378,6 @@ moves_loop:  // When in check, search starts here
 
         // Add extension to new depth
         newDepth += extension;
-        ss->multipleExtensions = (ss - 1)->multipleExtensions + (extension >= 2);
 
         // Speculative prefetch as early as possible
         prefetch(TT.first_entry(pos.key_after(move)));
