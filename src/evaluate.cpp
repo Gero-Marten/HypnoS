eb26bd0f91affa6ad8164e80a62b8d1e3a4b2c0a
Zerbinati <zerbinati.marco@gmail.com>
2024-07-24 15:14:07 +0200

    Replace simple eval with psqt in re-eval condition
    As a result, re-eval depends only on smallnet outputs
    so an extra call to simple eval can be removed.
    
    linrock authored

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -67,8 +67,7 @@ Value evaluate(const Eval::NNUE::Networks&    networks,
 
     assert(!pos.checkers());
 
-    int  simpleEval = simple_eval(pos, pos.side_to_move());
-    bool smallNet   = use_smallnet(pos);
+    bool smallNet = use_smallnet(pos);
     int  v;
 
     auto [psqt, positional] = smallNet ? networks.small.evaluate(pos, &caches.small)
@@ -78,7 +77,7 @@ Value evaluate(const Eval::NNUE::Networks&    networks,
 
 
     // Re-evaluate the position when higher eval accuracy is worth the time spent
-    if (smallNet && (nnue * simpleEval < 0 || std::abs(nnue) < 227))
+    if (smallNet && (nnue * psqt < 0 || std::abs(nnue) < 227))
     {
         std::tie(psqt, positional) = networks.big.evaluate(pos, &caches.big);
         nnue                       = (125 * psqt + 131 * positional) / 128;
