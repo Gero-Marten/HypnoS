c4914038e0cda06784a65cda6256209aa174a3fb
Zerbinati <zerbinati.marco@gmail.com>
2024-07-24 15:09:43 +0200

    Adding LowestElo and HighestElo constants
    These values represent the lowest Elo rating in the skill level calculation,
    and the highest one, but it's not clear from the code where these values come
    from other than the comment.  This should improve code readability and
    maintainability. It makes the purpose of the values clear and allows for easy
    modification if the Elo range for skill level calculation changes in the
    future.  Moved the Skill struct definition from search.cpp to search.h header
    file to define the Search::Skill struct, making it accessible from other files.
    
    FauziAkram authored

--- src/engine.cpp
+++ src/engine.cpp
@@ -96,7 +96,9 @@ Engine::Engine(std::string path) :
     options["nodestime"] << Option(0, 0, 10000);
     options["UCI_Chess960"] << Option(false);
     options["UCI_LimitStrength"] << Option(false);
-    options["UCI_Elo"] << Option(1320, 1320, 3190);
+    options["UCI_Elo"] << Option(Stockfish::Search::Skill::LowestElo,
+                                 Stockfish::Search::Skill::LowestElo,
+                                 Stockfish::Search::Skill::HighestElo);
     options["UCI_ShowWDL"] << Option(true);
 
     for (int i = 0; i < BookManager::NumberOfBooks; ++i)
